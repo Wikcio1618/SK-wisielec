53cbe2d1b3de2c5f0b88d8bf3ae66d68135686eb
Wikcio1618 <ciolekwiktor@gmail.com>
2023-05-03 17:01:20 +0200

    second commit

--- server.py
+++ server.py
@@ -1,20 +1,37 @@
 import socket
 from sys import argv
+from random import choice
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 1500
+wordsList = ["KUKURYDZA", "SAMOLOCIK", "OCZYSZCZALNIA"] # only one-word expressions supported 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(answer(data))
+def main():
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.bind((HOST, PORT))
+		s.listen()
+		conn, addr = s.accept()
+		with conn:
+			print(f"Connected by {addr}")
+			initGame()
+			conn.sendall(invitationMessage(addr))
+			while True:
+				data = conn.recv(1024)
+				if not data:
+					break
+				conn.sendall(feedback(data))
 			
-def answer(data):
+def feedback(data):
 	return data
+
+def initGame():
+	global WORD = choice(words)
+	
+def invitationMessage(addr)
+	return r"""
+				***********************************************
+				** Hello {addr}! The game is on. Good luck!! **
+				***********************************************
+				
+				The word: {WORD}
+			"""
